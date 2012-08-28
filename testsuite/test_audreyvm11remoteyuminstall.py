dbdae361feefa8446bcb60ab48a1d1db94dd6631
Milan Falešník <mfalesni@redhat.com>
2012-08-28 16:04:59 +0200

    I will smash my head

--- testsuite/test_audreyvm11remoteyuminstall.py
+++ testsuite/test_audreyvm11remoteyuminstall.py
@@ -32,4 +32,4 @@ def test_install_packages_remote(audreyvars, system_uuid):
         packages = packages.split(" ")
     else:
         pytest.skip(msg="No packages marked for remote install")
-    common.install_yum_package_remote(server, system_uuid, login, password, packages)
+    common.install_yum_packages_remote(server, system_uuid, login, password, packages)
