1b77337bcd99489d77f7df5aa1466125ee8fc974
Milan Falešník <mfalesni@redhat.com>
2012-09-14 16:47:39 +0200

    Correction, maybe it worksL

--- rhui-tests/test.sh
+++ rhui-tests/test.sh
@@ -14,9 +14,8 @@ echo "`date`: Running test ${1}"
 LOGFILE="`./lotr -r -y -v "${1}" | grep \"^LOGFILE: \" | sed -r -e \"s/^LOGFILE: //\"`"
 echo "Parsing log file $LOGFILE"
 cat $LOGFILE >&2
-FAILED="`cat $LOGFILE | grep \"FAIL\"`"
-SUCCESS="`echo $FAILED | wc -l`"
-if [ $SUCCESS -ne 0 ] ; then
+FAILS="`cat $LOGFILE | grep \"FAIL\"`"
+if [ -z "${FAILS}" ] ; then
   echo "FAILED: ${1} !"
   exit 1
 else
