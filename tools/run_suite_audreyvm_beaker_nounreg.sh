de7b7a60d4e84cd232b1b8cc09d06ff2cfc60954
Milan Falešník <mfalesni@redhat.com>
2012-09-18 13:35:26 +0200

    Added script for launching beaker tasks

---
+++ tools/run_suite_audreyvm_beaker_nounreg.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Specify SUITE_DIR to be used in script
+export SUITE_DIR="cloud-app-sanity"
+# Get the download script, which downloads and extracts repo tarball into $SUITE_DIR
+curl https://raw.github.com/mfalesni/cloud-app-sanity/master/tools/download_suite.sh | bash
+cd $SUITE_DIR && {
+  TESTNAME="audreyvm -unregister" make test  # Do the dirty stuff
+  TESTNAME="beaker" make test # Test the beaker tests
+}
