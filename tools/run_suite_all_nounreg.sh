8f63e316d83dac0da0ae074ed23faf9bdb02a4a5
Milan Falešník <mfalesni@redhat.com>
2012-09-27 13:14:08 +0200

    Added mail notification

--- tools/run_suite_all_nounreg.sh
+++ tools/run_suite_all_nounreg.sh
@@ -6,4 +6,17 @@ curl https://raw.github.com/mfalesni/cloud-app-sanity/master/tools/download_suit
 cd $SUITE_DIR && {
   TESTNAME="audreyvm -unregister" make test  # Do the dirty stuff
   TESTNAME="universal -beaker" make test  # Do the dirty stuff
+  make results.tar.gz
+  [ -n "$AUDREY_VAR_KATELLO_REGISTER_NOTIFY_EMAIL" ] && {
+    cd results-dir
+    touch result
+    for f in *.log
+        echo "[$f]" > result
+        cat $f > result
+        echo "" > result
+    done
+    cat result | mail -s "task finished on $HOSTNAME" "$AUDREY_VAR_KATELLO_REGISTER_NOTIFY_EMAIL"
+    rm -f result
+    cd ..
+    }
 }
