09894789c0ac145911d4fd1786c7afd0b4aa98ba
James Laska <jlaska@redhat.com>
2012-10-18 10:35:22 -0400

    Make mailer.sh more bulletproof

--- tools/mailer.sh
+++ tools/mailer.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
-[ -n "$AUDREY_VAR_KATELLO_REGISTER_NOTIFY_EMAIL" ] && {
-    cd results-dir
-    touch result
-    for f in *.log
-    do
-        echo "[$f]" >> result
-        cat $f >> result
-        echo "" >> result
-    done
-    cat result | mail -s "task finished on $HOSTNAME" "$AUDREY_VAR_KATELLO_REGISTER_NOTIFY_EMAIL"
-    rm -f result
-    cd ..
-    }
+RESULTS_DIR="results"
+if [ ! -d "$RESULTS_DIR" ]; then
+    echo "Skipping.  Missing results directory: $RESULTS_DIR"
+    exit 1
+fi
 
-make results.tar.gz
+case "$AUDREY_VAR_KATELLO_REGISTER_NOTIFY_EMAIL" in
+    *@*)
+        body=$(mktemp "$RESULTS_DIR/mail.XXXXXX")
+        for f in "$RESULTS_DIR"/*.log
+        do
+            echo "[$f]" >> "$body"
+            cat $f >> "$body"
+            echo "" >> "$body"
+        done
+        cat "$body" | mail -s "Task finished on $HOSTNAME" "$AUDREY_VAR_KATELLO_REGISTER_NOTIFY_EMAIL"
+        rm -f "$body"
+        ;;
+    *)
+        echo "Skipping.  No valid email provided ('$AUDREY_VAR_KATELLO_REGISTER_NOTIFY_EMAIL')"
+        echo 1
+        ;;
+esac
