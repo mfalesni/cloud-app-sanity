d777af7c1a93acc678910e2c4ebf93c7ef21c3a4
Milan Falešník <mfalesni@redhat.com>
2013-08-20 14:35:23 +0200

    RPM Package tests grouped.
    Tests using common input for RPM (e.g. package list) are now grouped together

---
+++ testsuite/test_RPMPackages.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2
+#   Author(s): Milan Falesnik <mfalesni@redhat.com>
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2013 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+    This file contains class which tests each RPM package in the system.
+"""
+
+import pytest
+import common.rpm
+import conftest as fixtures
+
+@pytest.mark.parametrize("package", fixtures.rpm_package_list())
+class TestRPM(object):
+    def test_sig_files(self, package):
+        """ This test checks a package whether it has signature and also whether all files are ok.
+            It also checks the return code of rpm -Vvv.
+
+        :param package: package to be checked
+        :type package: ``str``
+
+        :raises: pytest.Failed
+        """
+        problems = common.rpm.verify_package(package)
+        if not len(problems) == 0:
+            pytest.fail(msg="Package %s had following problems: '%s'" % (package, ", ".join(problems)), pytrace=False)
+
+    def test_fortified(self, package):
+        """ This test checks whether are all compiled files in package fortified.
+
+        :param package: Package name
+        :type package: ``list``
+
+        :raises: pytest.Failed
+        """
+
+        # FIXME - Review rpm-chksec
+        # (http://people.redhat.com/sgrubb/files/rpm-chksec) coverage to determine
+        # whether adjustments/enhancements are needed
+
+        files = common.rpm.ql(package).strip().split("\n")
+        for f in files:
+            if common.elf.is_elf(f):
+                dangerous = common.elf.fortify_find_dangerous(f)
+                for function in dangerous:
+                    if not function.endswith("_chk") and not function.endswith("__chk_fail"):
+                        pytest.fail(msg="File %s: Symbol '%s' found! All relevant symbols in file: %s" % (f, function, dangerous))
