f9c285ff0eab1697b9de092a64599236bf90c1ea
Milan Falešník <mfalesni@redhat.com>
2013-09-04 16:56:30 +0200

    sys and net tests added ...

---
+++ testsuite/test_NETWORK.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+#   Author(s): Milan Falesnik <mfalesni@redhat.com>
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2013 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+    This module contains tests targeted on system hardware and so.
+"""
+
+import pytest
+import common.shell
+import re
+
+#TODO parametrize externally
+@pytest.mark.parametrize(("rule"),[
+    ("ACCEPT", "tcp", "--", "anywhere", "anywhere", "state NEW tcp dpt:ssh"),
+    ("ACCEPT", "tcp", "--", "anywhere", "anywhere", "state NEW tcp dpt:http"),
+    ("ACCEPT", "tcp", "--", "anywhere", "anywhere", "state NEW tcp dpt:https"),
+    ])
+def test_iptables_rules(rule):
+    ''' Verifies key iptable rules are in place '''
+    iptables = common.shell.Run.command('iptables -L')
+    assert iptables
+    iptables = [re.sub(r"\s\s+", "\t", x) for x in iptables.stdout.strip().split("\n")]
+    found = False
+    for line in iptables:
+        line = tuple(x.strip() for x in line.split("\t"))
+        if line != rule:
+            continue
+        else:
+            found = True
+            break
+        
+
+    assert found
---
+++ testsuite/test_SYSTEM.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+#   Author(s): Milan Falesnik <mfalesni@redhat.com>
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2013 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+    This module contains tests targeted on system hardware and so.
+"""
+
+import pytest
+import common.shell
+import re
+
+
+def test_memory():
+    """
+        This test tests whether the box has at least 6G of memory
+
+    :raises: ``AssertionError``
+    """
+    mem = common.shell.Run.bash("free -g | grep Mem:")
+    assert mem
+    mem = re.sub(r"\s+", "\t", mem.stdout.strip()).split("\t")  # ['Mem:', '1', '1', ...]
+    mem = int(mem[1]) # second column
+    assert mem >= 6, "Machine must have at least 6G of mem."
+
+def test_cpu():
+    """
+        This test verifies that the machine has at least 4 cpus
+
+    :raises: ``AssertionError``
+    """
+    cpus = common.shell.Run.bash("lscpu | grep ^CPU\(s\):")
+    assert cpus
+    cpus = re.sub(r"\s+", "\t", cpus.stdout.strip())
+    cpus = int(cpus.split("\t", 1)[-1]) # Second column
+    assert cpus >= 4, "Machine must have at least 4 CPUs"
