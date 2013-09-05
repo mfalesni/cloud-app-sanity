e13f9fa4b363cc9127848b52068182e0458497f1
Milan Falešník <mfalesni@redhat.com>
2013-09-05 11:14:09 +0200

    Added additional service checks for cfme

--- testsuite/test_SERVICES.py
+++ testsuite/test_SERVICES.py
@@ -83,3 +83,31 @@ class TestServices(object):
             else:
                 state_message = "inactive"
             pytest.fail(msg="Service %s is not %s in runlevel %d!" % (service, state_message, runlevel))
+
+    def test_httpd_running(self):
+        """
+            Checks whether httpd service is running
+
+        :raises: ``AssertionError``
+        """
+        service = common.shell.Run.bash("service httpd status")
+        assert "is running" in service.stdout, "httpd must be running"
+
+    def test_evm_running(self):
+        """
+            Checks whether EVM service is running
+
+        :raises: ``AssertionError``
+        """
+        service = common.shell.Run.bash("service evmserverd status | grep EVM")
+        assert "started" in service.stdout, "evmserverd must be running"
+    
+    def test_iptables_running(self):
+        """
+            Checks whether iptables service is running
+
+        :raises: ``AssertionError``
+        """
+        service = common.shell.Run.bash("service iptables status")
+        assert "is not running" not in service.stdout, "iptables must be running"
+    
