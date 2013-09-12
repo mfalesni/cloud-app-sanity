b3fc784bcadbeb370b633ac269fc23f11c6600a0
Milan Falešník <mfalesni@redhat.com>
2013-09-12 10:16:45 +0200

    Unittestzero in pluginproxy

--- testsuite/plugins/__init__.py
+++ testsuite/plugins/__init__.py
@@ -33,6 +33,9 @@ class PluginProxy(object):
         self.Fail = pytest.fail
         self.Skip = pytest.skip
         self.Fixture = pytest.fixture
+        # unittestzero
+        unittestzero = __import__("unittestzero")
+        self.Assert = unittestzero.Assert
 
     def __getattribute__(self, attribute):
         try:
